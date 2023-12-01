abc82c34fe91dcfc6203549a4bbf3ca01e3d6d12
marktouch <marcellatouch@comcast.net>
2023-12-01 00:43:03 -0800

    insert link to go back to the current day

--- js/dailyGrind.js
+++ js/dailyGrind.js
@@ -13,16 +13,31 @@ let myDay = myDate.getDay();
 let today = "";
 let coffee = "";
 
+const queryString = window.location.search;
+    
+//output to console    
+console.log(queryString);
+    
+//separate query string parameters
+const urlParams = new URLSearchParams(queryString);
+if(urlParams.has("day")){//from querystring
+    myDay = urlParams.get("day");
+}
+
+myDay = parseInt(myDay);
+
+
+
 switch(myDay){
     case 0:
         today = "Sunday";
         coffee = {
-            name: "Bubble Tea",
-            pic: "images/bubble-tea.jpg",
-            alt: "a picture of a bubble tea",
-            color: "pink",
-            day: "Tuesday",
-            desc: `I like me some Bubble Tea!`
+            name: "Pumpkin Spice Latte",
+            pic: "images/pumpkin-spice-latte.jpg",
+            alt: "a picture of a pumpkin spice latte",
+            color: "orange",
+            day: "Sunday",
+            desc: `I like me some PSL!`
         };
     break;
 
@@ -32,7 +47,7 @@ switch(myDay){
             name: "Caramel Latte",
             pic: "images/caramel-latte.jpg",
             alt: "a picture of a caramel latte",
-            color: "brown",
+            color: "BurlyWood",
             day: "Monday",
             desc: `I like me some caramel latte!`
         };
@@ -56,7 +71,7 @@ switch(myDay){
             name: "Cold Brew",
             pic: "images/cold-brew.jpg",
             alt: "a picture of a cold brew",
-            color: "green",
+            color: "CornflowerBlue",
             day: "Wednesday",
             desc: `I like me some Cold Brew!`
         };
@@ -80,7 +95,7 @@ switch(myDay){
             name: "Frappaccino",
             pic: "images/frappaccino.jpg",
             alt: "a picture of a frappaccino",
-            color: "purple",
+            color: "green",
             day: "Friday",
             desc: `I like me some Frappaccino!`
         };
@@ -92,7 +107,7 @@ switch(myDay){
             name: "Mocha",
             pic: "images/mocha.jpg",
             alt: "a picture of a mocha",
-            color: "yellow",
+            color: "DarkGrey",
             day: "Saturday",
             desc: `I like me some Mocha!`
         };
@@ -104,7 +119,6 @@ switch(myDay){
 }
 
 
-console.log(today);
 // inject coffee data into page
 document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
 document.querySelector("html").style.backgroundColor = coffee.color;
